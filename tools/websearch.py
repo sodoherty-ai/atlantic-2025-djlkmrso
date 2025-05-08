3a42f3dba09a666330f49300269139fccfdd2600
sodoherty-ai <sodoherty@gmail.com>
2025-05-08 07:54:54 +0100

    fixed rate limit handling.

--- tools/websearch.py
+++ tools/websearch.py
@@ -1,3 +1,4 @@
+from duckduckgo_search.exceptions import DuckDuckGoSearchException
 from langchain_community.tools import DuckDuckGoSearchResults
 from crewai.tools import tool
 
@@ -8,14 +9,21 @@ def web_search(query: str, max_results: int = 10) -> list:
 
     Args:
         query (str): The search query.
-        max_results (int, optional): Maximum number of search results to return. Defaults to 10.
+        max_results (int, optional): Maximum number of search results to return (maximum is 10).
+
+    Returns:
+        An array of dictionaries. If the array is empty, then no results were found, or an error occurred.
     """
 
     print('Using Tool: DuckDuckGo Search')
 
-    response = DuckDuckGoSearchResults(
-        max_results=max_results,
-        output_format="list"
-    ).run(query)
+    if max_results > 10: max_results = 10
+    try:
+        response = DuckDuckGoSearchResults(
+            max_results=max_results,
+            output_format="list"
+        ).run(query)
+    except DuckDuckGoSearchException as e: # To handle rate limits.
+        response = []
 
     return response
