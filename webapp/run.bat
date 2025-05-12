79cc21278621b740bf98ec312168032d1fbf450e
sodoherty-ai <sodoherty@gmail.com>
2025-05-12 05:18:18 +0100

    Allow web app to run from command line without major refactoring of tree.

--- webapp/run.bat
+++ webapp/run.bat
@@ -5,6 +5,6 @@ set "CURRENT_DIR=%cd%"
 for %%i in ("%CURRENT_DIR%") do set "PARENT_DIR=%%~dpi"
 set "PARENT_DIR=%PARENT_DIR:~0,-1%"
 set "PYTHONPATH=%PARENT_DIR%;%CURRENT_DIR%;%PYTHONPATH%"
-uvicorn app:app --reload
+uvicorn main:app --reload
 
 endlocal
